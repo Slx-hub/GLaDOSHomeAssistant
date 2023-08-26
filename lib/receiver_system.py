c5b391874107f7eae59d4268f0af7952e9f0972a
Slakxs <schneja@tuta.io>
2023-08-26 11:27:23 +0200

    Added a healthcheck to shutdown before it's too late. Not working tho...

--- lib/receiver_system.py
+++ lib/receiver_system.py
@@ -1,5 +1,5 @@
 from lib.I_intent_receiver import Receiver, Reply
-import time, os
+import time, os, psutil
 from threading import Thread
 
 class System(Receiver):
@@ -10,6 +10,15 @@ class System(Receiver):
 		if(intent.intent == "Reboot"):
 			Thread(target=reboot).start()
 			return Reply(glados_path='special/shutdown')
+		if(intent.intent == "HealthCheck"):
+			return healthCheck()
+		
+def healthCheck():
+	print(psutil.virtual_memory().percent)
+	if psutil.virtual_memory().percent > 80.0:
+		Thread(target=reboot).start()
+		return Reply(glados_path='special/shutdown/good night.wav')
+	return Reply(neopixel_color=[0b00101010, 30, 0, 0, 0, 2])
 
 def shutdown():
 	time.sleep(10)
