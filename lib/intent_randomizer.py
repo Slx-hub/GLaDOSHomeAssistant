469b11a4c7c6abb4f36e7e8f1eb18f145d082ea2
Slx-hub <schneja@tuta.io>
2024-12-13 21:37:28 +0100

    too lazy to upgrade python

--- lib/intent_randomizer.py
+++ lib/intent_randomizer.py
@@ -5,6 +5,7 @@ def is_random_intent(intent):
     return '%-' in intent
     
 def roll_random_intent(intent):
-    if m := re.match(r'(\d+)%-(\w+)', intent):
+    m = re.match(r'(\d+)%-(\w+)', intent)
+    if m:
         val = random.randint(1,100)
         return m.group(2) if val <= int(m.group(1)) else None
