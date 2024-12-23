16b0af04585f33695fdfe9b934cc7ec1bfcbeda9
Slx-hub <schneja@tuta.io>
2024-12-23 16:03:47 +0100

    Whoops, kinda missed that keys must be unique

--- lib/scheduler.py
+++ lib/scheduler.py
@@ -1,24 +1,27 @@
 from time import sleep
 import schedule as sched
+import re
 
 def setup(callback, settings):
     sched.clear()
     if settings["daily"]:
-        for key, value in settings["daily"].items():
-            if key.startswith('<'):
-                continue
+        for key, value in sanitize_config_keys(settings["daily"]):
             schedule(sched.every().day.at(key), callback, value)
     if settings["hourly"]:
-        for key, value in settings["hourly"].items():
-            if key.startswith('<'):
-                continue
+        for key, value in sanitize_config_keys(settings["hourly"]):
             schedule(sched.every().hour.at(key), callback, value)
     if settings["everyXminutes"]:
-        for key, value in settings["everyXminutes"].items():
-            if key.startswith('<'):
-                continue
+        for key, value in sanitize_config_keys(settings["everyXminutes"]):
             schedule(sched.every(int(key)).minutes, callback, value)
     print("JOBS:", sched.get_jobs())
+
+def sanitize_config_keys(settings):
+    sanitized_map = {}
+    for key in settings.keys():
+        if key.startswith('<'):
+            continue
+        sanitized_map[re.sub("-\d", "", key)] = settings[key]
+    return sanitized_map
 
 def schedule(scheduler, callback, config_val):
     elements = config_val.split(">")
